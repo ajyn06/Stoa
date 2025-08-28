360eb2e8b9da2df2cd92475c77304b8d0c7ef1e4
Justyn Rubante <103063597+ajyn06@users.noreply.github.com>
2025-08-28 13:57:16 +1000

    Add slideshow navigation dots and enhance slideshow functionality

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 let slideIndex = 0;
+let slideTimer;
+
 showSlides();
 
 function showSlides() {
   let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1 }
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); 
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("active");
+
+  slideTimer = setTimeout(showSlides, 3000); 
+}
+
+function currentSlide(n) {
+  clearTimeout(slideTimer);
+  slideIndex = n - 1; 
+  showSlides();
 }
 
 const scrollElements = document.querySelectorAll(".animate-on-scroll, .collection-item");
